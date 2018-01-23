ceba57e4d5d9ec6b6cf77826031a1c71f5f16603
Brennan Heisler <learn@heislercreative.com>
2018-01-23 03:59:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  name_list = []
+  array.each { |name| name_list.push("Hello, my name is #{name}.")}
+  name_list
+end
+
+def assign_rooms(speakers)
+  room_assignment = []
+  speakers.each_with_index do |name, room|
+    room_assignment.push("Hello, #{name}! You'll be assigned to room #{room + 1}!")
+  end
+  room_assignment
+end
+
+def printer(batch_badge_creator(array), assign_rooms(speakers))
+  batch_badge_creator(array).each { |badge| puts badge }
+end
